5f00dddc8b79d497bdf4dcae47e03d56f0969373
Andrew Bateman <gomezbateman@yahoo.com>
2020-03-03 11:00:53 +0100

    initial commit

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 import { HomePage } from './home.page';
+import { SlideDrawerComponent } from '../slide-drawer/slide-drawer.component';
 
 @NgModule({
   imports: [
@@ -18,6 +19,6 @@ import { HomePage } from './home.page';
       }
     ])
   ],
-  declarations: [HomePage]
+  declarations: [HomePage, SlideDrawerComponent]
 })
 export class HomePageModule {}
---
+++ src/app/slide-drawer/slide-drawer.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-slide-drawer',
+  templateUrl: './slide-drawer.component.html',
+  styleUrls: ['./slide-drawer.component.scss'],
+})
+export class SlideDrawerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
